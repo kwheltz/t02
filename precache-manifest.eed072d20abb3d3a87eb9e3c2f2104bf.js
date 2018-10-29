6755590636135d35a1923f15d879bd4d6cf2524e
Yuri Garcia <kwheltz@gmail.com>
2018-10-29 11:09:43 -0300

    Updates

---
+++ precache-manifest.eed072d20abb3d3a87eb9e3c2f2104bf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "5406266b520421ddf0de705f3fa5062e",
+    "url": "/t02/static/media/Curriculum Vitae Yuri Garcia.5406266b.pdf"
+  },
+  {
+    "revision": "351198bad258b4c0e7e0",
+    "url": "/t02/static/css/main.20d0948a.chunk.css"
+  },
+  {
+    "revision": "ba28962a79cb906d2d07",
+    "url": "/t02/static/js/1.ba28962a.chunk.js"
+  },
+  {
+    "revision": "97b7c9de0faa9655122e",
+    "url": "/t02/static/js/runtime~main.97b7c9de.js"
+  },
+  {
+    "revision": "27a575e489b65ebd2f22b10f2c8ad463",
+    "url": "/t02/static/media/blog.27a575e4.jpg"
+  },
+  {
+    "revision": "682b4bfb4b135834d6a678175963133f",
+    "url": "/t02/static/media/paper.682b4bfb.jpg"
+  },
+  {
+    "revision": "e92a4f2a99f467167f0cab70d2c482da",
+    "url": "/t02/static/media/jobs.e92a4f2a.jpg"
+  },
+  {
+    "revision": "07dab97423d94b4dfc52ea0f985d4965",
+    "url": "/t02/static/media/carbon.07dab974.jpg"
+  },
+  {
+    "revision": "fba9733b651e574e588b6de8dedfce0f",
+    "url": "/t02/static/media/logo.fba9733b.png"
+  },
+  {
+    "revision": "8c900500c65b9e8b4da43c7c36c16ae0",
+    "url": "/t02/static/media/Avatar.8c900500.png"
+  },
+  {
+    "revision": "351198bad258b4c0e7e0",
+    "url": "/t02/static/js/main.351198ba.chunk.js"
+  },
+  {
+    "revision": "3fb620528534e4020602293a66af2364",
+    "url": "/t02/static/media/love icon.3fb62052.png"
+  },
+  {
+    "revision": "6d7d88cff066abc24c398af30a46cf4c",
+    "url": "/t02/static/media/chat icon.6d7d88cf.png"
+  },
+  {
+    "revision": "f08670fced6263165148d6aa76128cec",
+    "url": "/t02/static/media/activity icon.f08670fc.png"
+  },
+  {
+    "revision": "87dc016f9076b8facb2ab24a99b7c585",
+    "url": "/t02/static/media/tesla.87dc016f.jpg"
+  },
+  {
+    "revision": "2f7b25c7868db49e2ada8d198c9bd227",
+    "url": "/t02/static/media/work.2f7b25c7.png"
+  },
+  {
+    "revision": "ff1d322daa20d811d983e49ee7a0a4a3",
+    "url": "/t02/static/media/git.ff1d322d.png"
+  },
+  {
+    "revision": "6992f22f89babd3018d09555027b29cf",
+    "url": "/t02/static/media/fcb.6992f22f.png"
+  },
+  {
+    "revision": "8a5f1c35e7c456190be8f743dc302a4b",
+    "url": "/t02/static/media/insta.8a5f1c35.png"
+  },
+  {
+    "revision": "2770e1fd83b52f83a2cd8ad18e468d3f",
+    "url": "/t02/static/media/linkedin.2770e1fd.png"
+  },
+  {
+    "revision": "bb3c06b7c20ed0e0818b19b1399f6224",
+    "url": "/t02/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a0c7963d063b18b535a6512afafb7d1.js"
+  "/t02/precache-manifest.eed072d20abb3d3a87eb9e3c2f2104bf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/t02/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
